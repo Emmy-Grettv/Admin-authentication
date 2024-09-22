05494c34010d3ea4a342781ab47b491129adcb32
Emmy Gretta <karabogretta@gmail.com>
2024-09-22 20:55:13 +0200

    Environment setup

---
+++ server/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+//1. MIDDLEWARES
+app.use(express,json());
+
+//2. ROUTE
+
+//3. MONGO DB CONNECTION
+
+//4. GLOBAL ERROR HANDLER
+
+//5. SERVER
